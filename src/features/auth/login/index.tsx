302d8b21547ee67202c122c8367a397206c2210f
Ozami Duque Rodriguez <ozamiduro.dev@gmail.com>
2024-09-23 15:54:42 -0500

    Add pages

---
+++ src/features/auth/login/index.tsx
@@ -0,0 +1,9 @@
+const Login = () => {
+  return (
+    <>
+      <h1>Here will be the Login feature</h1>
+    </>
+  );
+};
+
+export { Login };
---
+++ src/pages/login/index.tsx
@@ -0,0 +1,13 @@
+import { Login } from "../../features/auth/login";
+
+const LoginPage = () => {
+  return (
+    <>
+      <h1>Manhattan Project</h1>
+      <p>Building...</p>
+      <Login />
+    </>
+  );
+};
+
+export default LoginPage;

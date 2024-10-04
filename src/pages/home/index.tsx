58f2a749b73f9437cfc79c6b00d844d8089cb3b5
Jonathan-Buitrago <jonathanbuitrago505@gmail.com>
2024-10-04 18:36:06 -0500

    Add fisrt config for add location button

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,9 +1,11 @@
+import AddLocationButton from "../../features/ui/addLocationButton";
 import { Header } from "../../features/ui/header";
 
 const HomePage = () => {
   return (
     <>
       <Header />
+      <AddLocationButton />
       <h1>Home</h1>
     </>
   );
